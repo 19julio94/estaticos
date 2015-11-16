711d2d50fdcd98319b348d644282934fcfb153bd
jpatriciodasilva <jpatriciodasilva@atanasoff03.danielcastelao.org>
2015-11-16 09:11:24 +0100

    inicio

---
+++ src/exemploestaticos/ExemploEstaticos.java
@@ -0,0 +1,16 @@
+
+
+package exemploestaticos;
+
+/**
+ *
+ * @author jpatriciodasilva
+ */
+public class ExemploEstaticos {
+
+    
+    public static void main(String[] args) {
+       
+    }
+    
+}
---
+++ src/exemploestaticos/LibretaDeAforros.java
@@ -0,0 +1,36 @@
+
+
+package exemploestaticos;
+
+/**
+ *
+ * @author jpatriciodasilva
+ */
+public class LibretaDeAforros {
+    private float cartos;
+    private static float xuros ;
+    
+    public LibretaDeAforros(){
+        
+    }
+public LibretaDeAforros(float cart,float xur){
+    cartos=cart;
+    xuros=xur;
+    
+    
+}    
+public void setcartos(float sal){
+    
+}
+public float getcartos(){
+    return cartos; 
+    
+}
+    public void setxuro(float xur){
+        
+    }
+    public float getxuros(){
+        return xuros;
+    }
+    
+}
